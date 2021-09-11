9c03b2a4988b5c6467813da72e75c9cfb61d3ab5
rootChristian <softwarehristian1@gmail.com>
2021-09-12 00:30:57 +0200

    add server

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
-
-// import routes
-const authRoutes = require("./routes/auth");
 
 // app
 const app = express();
@@ -27,7 +25,7 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
 // routes middleware
-app.use("/api", authRoutes);
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
